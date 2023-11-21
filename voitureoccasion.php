63dba397e5350d1519375de9bbc54990facf9071
Romaric NGANAWARA <130139612+touandai@users.noreply.github.com>
2023-11-21 18:06:24 +0100

    Update voitureoccasion.php

--- voitureoccasion.php
+++ voitureoccasion.php
@@ -15,7 +15,7 @@
                 <nav class="navbar">
                     <ul>
                        <li class="btn-lg btn btn-danger"><a href="index.php">Services</a>  
-                       <li class="btn-lg btn btn-danger"><a href="voitureoccasion.php">Véhicules d'occasion</a></li>
+                       <li class="btn-lg btn btn-danger"><a href="voitureoccasion.html">Véhicules d'occasion</a></li>
                        <li class="btn-lg btn btn-danger"><a href="rendezvous.php">Rendez-vous</a></li>
                        <li class="btn-lg btn btn-danger"><a href="contact.php">Contact</a></li>
                        <li class="btn-lg btn btn-danger"><a href="compteperso.php">Mon compte</a></li>
